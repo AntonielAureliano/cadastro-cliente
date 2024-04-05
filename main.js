636e478b6642d4d171a40b1ef06523f9d4e01184
Antoniel Aureliano <tonielaureliano@gmail.com>
2024-04-05 19:40:38 -0300

    Criando updateClient e deleteClient.

--- main.js
+++ main.js
@@ -7,15 +7,33 @@ const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
 const tempClient = {
-    nome: "Antoniel",
-    email: 'antoniel@gmail.com',
-    celular: '63991246060',
-    cidade: 'Colinas - To'
+    nome: "Nami",
+    email: 'nami@gmail.com',
+    celular: '6300000000',
+    cidade: 'Vila Cocoyasi'
 }
 
 //creat
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
 const setLocalStorage = (dbClient) => localStorage.setItem('db_client', JSON.stringify(dbClient));
+
+//read
+const readClient =  () => getLocalStorage();
+
+
+//update
+const updateClient = (index, client) => {
+    const dbClient = readClient();
+    dbClient[index] = client;
+    setLocalStorage(dbClient);
+}
+
+//delete
+const deleteClient = (index) => {
+    const dbClient = readClient();
+    dbClient.splice(index, 1);
+    setLocalStorage(dbClient);
+}
 
 //event
 const createClient = (client) =>  {
